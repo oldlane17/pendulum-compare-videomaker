fdf1a5be3e5ddb9c7963b982c13e562e286d636f
Old Lane 17 <oldlane18@gmail.com>
2025-09-28 17:12:45 +0200

    Create init.py

---
+++ pendulum/init.py
@@ -0,0 +1,9 @@
+"""
+Pendulum Simulator Package
+"""
+
+from .simulator import PendulumSimulator
+from .animator import create_animation
+
+__version__ = "1.0.0"
+__all__ = ['PendulumSimulator', 'create_animation']
